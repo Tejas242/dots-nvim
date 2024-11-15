c3c6fa7b7254ac14dfc92dfede3f1f57703ca57a
screenager <tmahajan09876@gmail.com>
2024-11-15 09:14:06 +0530

    Added all of CP

---
+++ cp/templates/template.cpp
@@ -0,0 +1,31 @@
+/*
+ * Author: @screenager
+ * Start: $(DATE)
+ */
+ 
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef DEBUG
+#include "../algo/debug.h"
+#else
+#define debug(...) 42
+#endif
+ 
+typedef long long ll;
+ 
+void solve() {
+
+}
+ 
+int main () {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int t;
+  cin >> t;
+  while(t--){
+    solve();
+  }
+ 
+  return 0;
+}
